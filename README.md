2e16d8c12182eb0900cba7683d4097a3568c090b
Chandan Prasad <cp564738@gmail.com>
2019-08-08 11:58:38 +0530

    02 Get Route Done

--- README.md
+++ README.md
@@ -7,3 +7,13 @@
 ---
 * ```pip install Flask==1.1.1```
 
+
+## Routes
+
+---
+
+### GET route
+* ```/books``` => return all books
+* ```/books/809``` => return book of isbn 809
+
+
--- src/app.py
+++ src/app.py
@@ -1,10 +1,32 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
-print(__name__)
 
-@app.route("/")
-def hello_world():
-    return "Hello World"
+books = [
+    {
+        "name" : "Book1",
+        "price" : 3,
+        "isbn" : 908,
+    },
+    {
+        "name" : "Book2",
+        "price" : 4,
+        "isbn" : 89,
+    }
+]
+
+@app.route("/books")
+def get_books():
+    return jsonify({"books" : books})
+
+@app.route("/books/<int:isbn>")
+def get_books_by_isbn(isbn):
+    for book in books:
+        if(book["isbn"] == isbn):
+            return_value = {
+                "name" : book["name"],
+                "price" : book["price"]
+            }
+    return jsonify(return_value)
 
 app.run(port=5000)
